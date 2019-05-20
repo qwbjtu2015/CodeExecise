0ae9ed979403a49cfbaf2d3bd5cefce7a4266ca1
qwbjtu2015 <13251196@bjtu.edu.cn>
2019-05-20 14:10:12 +0800

    Create QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,27 @@
+/*
+* 快排
+* 最差时间复杂度O(N^2) 最佳时间复杂度O(NlogN) 平均时间复杂度O(NlogN)
+* 当所有数字顺序排列或逆序排列时 复杂度最大 因为树深最大（N）
+* 树深最小为logN， 因此最佳时间复杂度为O(NlogN)
+*/
+public class Main {
+	public static void qSort(int left, int right, int[] data) {
+		int middle = partation(left, right, data);
+		qSort(left, middle-1, data);
+		qSort(middle+1, right, data);
+	}
+
+	public static int partation(int left, int right, int[] data) {
+		int pivot = data[left];
+		while(left < right) {
+			while(left < right && data[right] >= pivot)
+				right--;
+			data[left] = data[right];
+			while(left < right && data[left] <= pivot)
+				left++;
+			data[right] = data[left];
+		}
+		data[left] = pivot;
+		return left;
+	}
+}
