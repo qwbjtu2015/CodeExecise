1168243e37da07c4e9c933e29bdc7587cea70fb4
qwbjtu2015 <qwbjtu15@163.com>
2019-05-20 14:25:42 +0800

    缩进格式

--- HeapSort.java
+++ HeapSort.java
@@ -1,12 +1,12 @@
-/*
-堆排序
-把调整堆这个功能抽离出来，通过递归调用实现堆的调整【maxHeap函数】
+/**
+ * 堆排序
+ * 把调整堆这个功能抽离出来，通过递归调用实现堆的调整【maxHeap函数】
  */
 public class Main {
-  public static void heapSort(int[] data) {
-    // 由原始数组构建堆
+  	public static void heapSort(int[] data) {
+		// 由原始数组构建堆
 		buildHeap(data);
-    // 每次将堆顶元素与当前堆的最后一个元素交换，然后缩小堆大小，并调整堆
+		// 每次将堆顶元素与当前堆的最后一个元素交换，然后缩小堆大小，并调整堆
 		for(int i = 0; i < data.length; i++) {
 			int temp = data[0];
 			data[0] = data[data.length - 1 - i];
@@ -22,12 +22,12 @@ public class Main {
 		}
 	}
 
-  /**
-   * 递归调用以调整堆
-   * @param data 数据数组
-   * @param heapSize 当前堆长度
-   * @param curr 要调整的位置
-   */
+	/**
+	* 递归调用以调整堆
+	* @param data 数据数组
+	* @param heapSize 当前堆长度
+	* @param curr 要调整的位置
+	*/
 	public static void maxHeap(int curr, int heapSize, int[] data) {
 		int left = curr*2+1;
 		int right = curr*2+2;
--- QuickSort.java
+++ QuickSort.java
@@ -1,9 +1,9 @@
-/*
-* 快排
-* 最差时间复杂度O(N^2) 最佳时间复杂度O(NlogN) 平均时间复杂度O(NlogN)
-* 当所有数字顺序排列或逆序排列时 复杂度最大 因为树深最大（N）
-* 树深最小为logN， 因此最佳时间复杂度为O(NlogN)
-*/
+/**
+ * 快排
+ * 最差时间复杂度O(N^2) 最佳时间复杂度O(NlogN) 平均时间复杂度O(NlogN)
+ * 当所有数字顺序排列或逆序排列时 复杂度最大 因为树深最大（N）
+ * 树深最小为logN， 因此最佳时间复杂度为O(NlogN)
+ */
 public class Main {
 	public static void qSort(int left, int right, int[] data) {
 		int middle = partation(left, right, data);
